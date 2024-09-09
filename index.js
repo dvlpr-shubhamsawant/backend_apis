6aac492e5b6026bea517347c5a330f22542fb8ab
shubham-n-sawant <shubhamsawant094@gmail.com>
2024-09-09 15:26:34 +0530

    post api added

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ import pg from 'pg';
 const app = express();
 app.use(express.json());
 
-
+const pool = new pg.Pool({
+    connectionString : process.env.POSTGRES_URL,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+});
 
 app.get("/",async(req, res)=>{
     try {
@@ -17,7 +23,21 @@ app.get("/",async(req, res)=>{
     })
 });
 
-
+app.post("/", async(req, res) => {
+    let client ;
+    try {
+        client = await pool.connect();
+        var resutl = client.query("CREATE TABLE user (name TEXT);");
+        console.log(resutl);
+        res.json({
+            "msg": "Tbale created successfully"
+        });
+        
+    } catch (error) {
+       console.log(error);
+        
+    }
+});
 
 app.listen(3000, ()=>{
     console.log("Port listening on 3000");
